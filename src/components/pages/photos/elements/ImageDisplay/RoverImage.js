a47744ed3e604ac4c31b7e673208c812b2ff0a64
Ebony Zhang <Ebony.Zhang@softwire.com>
2019-12-20 16:30:14 +0000

    styling

--- src/components/pages/photos/elements/ImageDisplay/RoverImage.js
+++ src/components/pages/photos/elements/ImageDisplay/RoverImage.js
@@ -1,58 +1,29 @@
 import React, { Component } from 'react';
-
 import '../../PhotoPage.css';
 import RoverImageButton from './RoverImageButton';
 
 export default class RoverImage extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      index: 0,
-      currentRover: 'Curiosity',
-    };
-  }
-
-  incrementIndex() {
-    this.setState(state => (
-      {
-        index: Math.min(state.index + 1, this.props.photoUrlArray.length - 1),
-      }
-    ));
-  }
-
-  decrementIndex() {
-    this.setState(state => (
-      {
-        index: Math.max(0, state.index - 1),
-      }
-    ));
-  }
-
   render() {
     return (
       <div>
-        <div className="image-and-intro">
-          <div className="rover-image">
-            <RoverImageButton
-              className="left-button"
-              direction="<"
-              onClick={() => this.decrementIndex()}
-            />
-            <img
-              className="camera-capture"
-              src={this.props.photoUrlArray[this.state.index]}
-              alt="Mars"
-            />
-            <RoverImageButton
-              className="right-button"
-              direction=">"
-              onClick={() => this.incrementIndex()}
-            />
-          </div>
+        <div className="rover-image">
+          <RoverImageButton
+            className="left-button"
+            direction="<"
+            onClick={() => this.props.decrementIndex()}
+          />
+          <img
+            className="camera-capture"
+            src={this.props.photoUrlArray[this.props.index]}
+            alt="Mars"
+          />
+          <RoverImageButton
+            className="right-button"
+            direction=">"
+            onClick={() => this.props.incrementIndex()}
+          />
         </div>
-        <div className="photo-index">
-          <h4>Photo {this.state.index + 1} / {this.props.photoUrlArray.length}</h4>
-        </div>
+        <h4 className="photo-count">Photo {this.props.index + 1} / {this.props.photoUrlArray.length}</h4>
       </div>
     );
   }
