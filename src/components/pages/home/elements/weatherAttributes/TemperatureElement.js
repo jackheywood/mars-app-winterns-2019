9fc33b785edd782855583c32df4b320a8bad831c
Kris Gwynne <krisgwynne@ymail.com>
2019-12-20 12:15:41 +0000

    formatted the weather component partially

--- src/components/pages/home/elements/weatherAttributes/TemperatureElement.js
+++ src/components/pages/home/elements/weatherAttributes/TemperatureElement.js
@@ -5,9 +5,11 @@ export default class TemperatureElement extends Component {
     return (
       <div className="temperature-element">
         <h2>Temperature</h2>
-        <h5>Avg: {`${this.props.temp.avg}\u2103`} </h5>
-        <h5>Max: {`${this.props.temp.max}\u2103`} </h5>
-        <h5>Min: {`${this.props.temp.min}\u2103`} </h5>
+        <div>
+          <h5>Avg: {`${this.props.temp.avg}\u2103`} </h5>
+          <h5>Max: {`${this.props.temp.max}\u2103`} </h5>
+          <h5>Min: {`${this.props.temp.min}\u2103`} </h5>
+        </div>
       </div>
     );
   }
